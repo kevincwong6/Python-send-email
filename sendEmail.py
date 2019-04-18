a41749e76750807bf424e779adbffe469bd85168
Kevin Wong <48312245+kevincwong6@users.noreply.github.com>
2019-04-18 01:18:35 -0700

    converted it to API

--- sendEmail.py
+++ sendEmail.py
@@ -3,10 +3,6 @@ import smtplib
 
 #######################################################################
 #
-# USAGE: python sendEmail.py recipientEmail subject emailBody
-#
-#######################################################################
-#
 # Step 1: Disable 2-step verification:
 # https://myaccount.google.com/security?utm_source=OGB&utm_medium=act#signin
 #
@@ -15,47 +11,57 @@ import smtplib
 # 
 #######################################################################
 
+# ----------------------------- sendEmail -------------------------------------
+def sendEmail(
+  sender,
+  password,
+  smtp_server,
+  recipient,
+  subject,
+  message_body):
+
+  message="Subject: "+subject+"\n"+message_body
+
+  print("\nFrom   : "+sender)
+  print("To     : "+recipient)
+  print("Subject: "+subject)
+  print("Message: "+message_body)
+
+  confirm=input("\n\nsend? (y)? ")
+  if ((len(confirm) != 0) and (confirm.lower() != 'y')):
+    exit()
+
+  try:
+    server=smtplib.SMTP(smtp_server, 587)
+    #server.set_debuglevel(1)
+    server.ehlo()
+    server.starttls()
+    server.login(sender, password)
+    server.sendmail(sender, recipient, message)
+    server.quit()
+    print("\n\nEmail Sent!\n\n")
+  except:
+    print("Failed to connect to email server")
+
+
+# ------------------------------- main ----------------------------------------
 if (len(sys.argv) != 4):
   print("\nUSAGE: python sendEmail.py recipientEmail subject emailBody\n\n")
   exit()
 
-recipient=sys.argv[1]
-subject=sys.argv[2]
-message_body=sys.argv[3]
-message="Subject: "+subject+"\n"+message_body
 
-PASSWORD="password"       # replace it with your password
-SENDER="sender@gmail.com" # replace it with your gmail account
+PASSWORD="password"         # replace it with your password
+SENDER="sender@gmail.com"   # replace it with your gmail account
 SMTP_SERVER="smtp.gmail.com"
-LOGIN=SENDER
-
-print("\nFrom   : "+SENDER)
-print("To     : "+recipient)
-print("Subject: "+subject)
-print("Message: "+message_body)
-
-confirm=input("\n\nsend? (y)? ")
-if ((len(confirm) != 0) and (confirm.lower() != 'y')):
-  exit()
-
-try:
-  server=smtplib.SMTP(SMTP_SERVER, 587)
-  #server.set_debuglevel(1)
-  server.ehlo()
-  server.starttls()
-  server.login(SENDER, PASSWORD)
-  server.sendmail(SENDER, recipient, message)
-  server.quit()
-  print("\n\nEmail Sent!\n\n")
-except:
-  print("Failed to connect to email server")
-
+sendEmail(SENDER, PASSWORD, SMTP_SERVER, sys.argv[1], sys.argv[2], sys.argv[3])
 
 #######################################################################
 #
 # Sample run:
 #
-# G:\python sendEmail.py yyyyy@yahoo.com testing5 "test 5 message"
+# USAGE: python sendEmail.py recipientEmail subject emailBody
+#
+# G:\> python sendEmail.py receiver@yahoo.com testing5 "test 5 message"
 #
 # From   : sender@gmail.com
 # To     : yyyyy@yahoo.com
